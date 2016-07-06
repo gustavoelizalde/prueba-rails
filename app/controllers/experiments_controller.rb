b1d0ef56a3ec7900f77f23c508ca8a5814b9ac53
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-05 20:12:14 -0400

    Punto 17,18 y 19

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,14 @@
 class ExperimentsController < ApplicationController
+
   def page1
+  	render :layout => 'layouts/bootstrap'
   end
 
   def page2
+  	render :layout => 'layouts/bootstrap'
   end
 
   def page3
+  	render :layout => 'layouts/bootstrap'
   end
 end
