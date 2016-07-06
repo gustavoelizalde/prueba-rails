306e0c4850eadf089f28de2d57da6b13bb360f73
gustavoelizalde <gustavoelizalde@gmail.com>
2016-07-05 20:02:25 -0400

    Puntos 12,13 y 14

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,21 @@ class PagesController < ApplicationController
   def batman_vs_superman
   	@nombre = "Gustavo"
   end
+
+  def vote
+  	
+  	if params[:heroe].present?
+  		heroe = Voto.new(heroe: params[:heroe], email: params[:email])
+  		if heroe.save
+  			redirect_to pages_batman_vs_superman_path,
+  				notice: "El heroe fue guardado exitosamente"
+  		else
+  			redirect_to pages_batman_vs_superman_path,
+  				notice: "Ocurrió un error guardado el heroe"
+  		end
+  	else
+  		redirect_to pages_batman_vs_superman_path
+  	end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/batman'
   get 'pages/superman'
   get 'pages/batman_vs_superman'
+  get 'pages/vote'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
